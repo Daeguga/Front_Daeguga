e9d5e79b5aad1a21242864d0fb1430ca5e51d01c
gye0n1 <134510838+gye0n1@users.noreply.github.com>
2024-09-08 08:11:28 +0900

    ⚡️:: main

--- daegu/src/Router.js
+++ daegu/src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from './Main/Main';
 import Login from './Login/Login';
 
