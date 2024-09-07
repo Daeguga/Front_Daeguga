61486329121c6dbe786e85672158745fc4eb1e77
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2024-09-07 23:10:06 +0900

    feat : 파일세팅

---
+++ daegu/src/Router.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Main from './Main/Main';
+import Login from './Login/Login';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/main' element={<Main />} />
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router;
