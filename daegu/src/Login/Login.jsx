7194fd3ddafc6d1eb97163f1b5549d1753b73212
gye0n1 <134510838+gye0n1@users.noreply.github.com>
2024-09-08 10:26:28 +0900

    ✨:: login view finish

--- daegu/src/Login/Login.jsx
+++ daegu/src/Login/Login.jsx
@@ -5,8 +5,12 @@ import * as L from "./Login.styled";
 const Login = () => {
   return (
     <>
-      <div>Login</div>
-      <L.logo img src={logo} />
+      <L.box>
+        <L.logo img src={logo} />
+        <L.id placeholder="아이디를 입력하세요"></L.id>
+        <L.password placeholder="비밀번호를 입력하세요"></L.password>
+        <L.login placeholder="로그인"></L.login>
+      </L.box>
     </>
   );
 };
